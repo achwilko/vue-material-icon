18a4f20b8a90464d66967063ae6e288ab8354045
achwilko <adam.chwilkowski@gmail.com>
2017-12-08 14:23:32 +0100

    updated unit tests

--- test/unit/specs/VueMaterialIcon.spec.js
+++ test/unit/specs/VueMaterialIcon.spec.js
@@ -3,30 +3,30 @@ import { shallow } from 'vue-test-utils'
 
 const name = 'favorite'
 const size = 60
+const component = shallow(VueMaterialIcon)
 
 describe('Component VueMaterialIcon', () => {
-  it('should be rendered', () => {
-    const component = shallow(VueMaterialIcon, { propsData: { name } })
-    expect(component.html()).not.toBe(undefined)
+  it('should not be rendered without prop name', () => {
+    expect(component.html()).toBeUndefined
   })
 
-  it('should not be rendered without prop name', () => {
-    const component = shallow(VueMaterialIcon)
-    expect(component.html()).toBe(undefined)
+  it('should be rendered with prop name', () => {
+    component.setProps({ name })
+    expect(component.html()).toBeDefined()
   })
 
   it('should get default prop size', () => {
-    const component = shallow(VueMaterialIcon, { propsData: { name } })
+    component.setProps({ name })
     expect(component.hasStyle('font-size', '24px')).toBe(true)
   })
 
   it('should get custom prop size', () => {
-    const component = shallow(VueMaterialIcon, { propsData: { name, size } })
+    component.setProps({ name, size })
     expect(component.hasStyle('font-size', '60px')).toBe(true)
   })
 
   it('should get custom prop name', () => {
-    const component = shallow(VueMaterialIcon, { propsData: { name } })
+    component.setProps({ name })
     expect(component.text()).toBe('favorite')
   })
 })
